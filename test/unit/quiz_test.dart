29fd55e6d61aaed971a6fae944f697eb666c347b
CaioLuppo <luppodecode@outlook.com>
2023-08-20 20:32:32 -0300

    test: quiz test

---
+++ test/unit/quiz_test.dart
@@ -0,0 +1,46 @@
+import "package:fast_trivia/model/quiz.dart";
+import "package:flutter_test/flutter_test.dart";
+
+void main() {
+  test("Verifies if returns a instance with a proper map", () {
+    final Map json = {
+      "questionario": {
+        "id": 1,
+        "titulo": "Natureza brasileira",
+        "questoes": [
+          {
+            "id": 1,
+            "titulo": "Florestas brasileiras",
+            "pergunta": "Qual o nome da maior floresta brasileira?",
+            "gabarito": 4,
+            "alternativas": [
+              {"id": 1, "titulo": "Caatinga"},
+              {"id": 2, "titulo": "Cerrado"},
+              {"id": 3, "titulo": "Mata Atlântica"},
+              {"id": 4, "titulo": "Amazônica"}
+            ]
+          },
+          {
+            "id": 2,
+            "titulo": "Praias brasileiras",
+            "pergunta": "Qual estado tem a maior faixa litorânea?",
+            "gabarito": 3,
+            "alternativas": [
+              {"id": 1, "titulo": "São Paulo"},
+              {"id": 2, "titulo": "Maranhão"},
+              {"id": 3, "titulo": "Bahia"},
+              {"id": 4, "titulo": "Rio de Janeiro"}
+            ]
+          }
+        ]
+      }
+    };
+    Object? quiz;
+
+    try {
+      quiz = Quiz(json["questionario"]);
+    } catch (_) {}
+
+    expect(quiz is Quiz, true);
+  });
+}
