bfc92830bd5bd4de921685469eb601705e90b42f
CaioLuppo <luppodecode@outlook.com>
2023-08-20 22:32:49 -0300

    style: tema e dependencias

---
+++ lib/view/resources/trivia_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TriviaColors {
+  static const Color red = MaterialColor(
+    0xFFDB3B28,
+    {
+      10: Color(0xdb3b281a),
+    },
+  );
+  static const Color green = MaterialColor(
+    0xFF619B0B,
+    {
+      10: Color(0x619b0b1a),
+    },
+  );
+  static const Color blue = Color(0xFF259FDB);
+  static const Color purple = Color(0xFF6675DB);
+  static const Color yellow = Color(0xFFDBA600);
+}
