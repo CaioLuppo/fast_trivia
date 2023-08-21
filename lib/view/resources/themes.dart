a2949d560e1befd8c9ad0745b426ac0aac79f79f
CaioLuppo <luppodecode@outlook.com>
2023-08-21 11:43:35 -0300

    style: cores

--- lib/view/resources/themes.dart
+++ lib/view/resources/themes.dart
@@ -1,7 +1,13 @@
+import 'package:fast_trivia/view/resources/trivia_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 final ThemeData lightTheme = ThemeData(
   appBarTheme: AppBarTheme(
     color: Colors.grey[50],
+    systemOverlayStyle: const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
   ),
+  cardColor: TriviaColors.cardBackground
 );
--- lib/view/resources/trivia_colors.dart
+++ lib/view/resources/trivia_colors.dart
@@ -16,4 +16,7 @@ class TriviaColors {
   static const Color blue = Color(0xFF259FDB);
   static const Color purple = Color(0xFF6675DB);
   static const Color yellow = Color(0xFFDBA600);
+
+  static const Color cardBackground = Color(0xFDFDFDFD);
+  static const Color subtitles = Color(0x99999999);
 }
